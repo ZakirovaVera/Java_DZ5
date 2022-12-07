4a323d7a8e8f5be6460e063ad45018ed73c8254f
ZakirovaVera <zakirovavk@gmail.com>
2022-12-08 00:34:49 +0500

    задача 2

--- task2.java
+++ task2.java
@@ -1,10 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 // Пусть дан список сотрудников:Иван Иванов ( и остальные, полный текст дз будет на
 // платформе)
 // Написать программу, которая найдет и выведет повторяющиеся имена с
 // количеством повторений. Отсортировать по убыванию популярности.
 
 public class task2 {
+    static Map<String, Integer> list = new HashMap<>();
+    static List<String> dataEmployees = new ArrayList<>();
+
     public static void main(String[] args) {
-        
+        dataEmployees.add("Сидоров");
+        dataEmployees.add("Сидоров");
+        dataEmployees.add("Петров");
+        dataEmployees.add("Иванов");
+        dataEmployees.add("Иванов");
+        dataEmployees.add("Сидоров");
+
+        CalcCountRepeats();
+
+        for (var el : list.entrySet()) {
+            System.out.print(el.getKey() + " " + el.getValue() + "\n");
+        }
+    }
+    public static void CalcCountRepeats() {
+        for (String el : dataEmployees) {
+            if(list.containsKey(el)){
+                int count = list.get(el);
+                count++;
+                list.put(el, count);
+            }
+            else{
+                list.put(el, 1);
+            }
+        }
     }
 }
