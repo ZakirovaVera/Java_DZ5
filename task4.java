fd74996849d68a714f8b43e6c809787ae4b7c713
ZakirovaVera <zakirovavk@gmail.com>
2022-12-09 20:55:04 +0500

    задача не решена

--- task4.java
+++ task4.java
@@ -5,7 +5,7 @@ public class task4 {
     static int numberQueens = N;
     static int chessBoard [][] = new int [N][N];
     public static void main(String[] args) {
-        
+        SetQueen();
         ShowChessboard(); // показать доску
 
     }
@@ -16,10 +16,35 @@ public class task4 {
             {
                 for (int j = 0; j < N; ++j)
                 {
-                    // если в массиве элемент = 1 печатать "*", иначе "."
                     System.out.print((chessBoard[i][j]==1) ? "* " : ". ");
                 }
                 System.out.print('\n');
             }
+    }
+    // установить ферзя
+    public static void SetQueen() {
+       int a = 0;
+        for (int j = 0; j < N; j++) {
+                if (CheckQueen(a, j)){
+                    chessBoard[a][j] = 1;
+                    a++;
+                }
+        }
+    }
+    // проверить ферзя
+    public static boolean CheckQueen(int row, int column) {
+        for (int i = 0; i < row; ++i)
+            if (chessBoard[i][column]==1)
+                return false;
+ 
+        for (int i = 1; i <= row && column - i >= 0; ++i)
+            if (chessBoard[row - i][column - i]==1)
+                return false;
+ 
+        for (int i = 1; i <= row && column + i < N; i++)
+            if (chessBoard[row - i][column + i]==1)
+                return false;
+ 
+        return true;
     }
 }
