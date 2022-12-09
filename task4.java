7c8d8e829f62e50636efbb6aeaa23198fef58e07
ZakirovaVera <zakirovavk@gmail.com>
2022-12-09 16:38:06 +0500

    метод покахать доску

--- task4.java
+++ task4.java
@@ -1,7 +1,25 @@
 // На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга.
 
 public class task4 {
+    static int N = 8;
+    static int numberQueens = N;
+    static int chessBoard [][] = new int [N][N];
     public static void main(String[] args) {
         
+        ShowChessboard(); // показать доску
+
+    }
+
+    // показать доску
+    public static void ShowChessboard() {
+        for (int i = 0; i < N; ++i)
+            {
+                for (int j = 0; j < N; ++j)
+                {
+                    // если в массиве элемент = 1 печатать "*", иначе "."
+                    System.out.print((chessBoard[i][j]==1) ? "* " : ". ");
+                }
+                System.out.print('\n');
+            }
     }
 }
